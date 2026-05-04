99d669f071ca38902c3684ed7d221dd7acf72450
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-04 12:31:44 +0700

    menambahkan iostream dan namespace std

--- this.cpp
+++ this.cpp
@@ -1,19 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class buku{
-    string judul;
-    public:
-        string setget(string judul){
-            this->judul=judul; //nilai parameter 'judul untuk member variabel
-            return this->judul; //return variabel judul.
-        }
-
-};
-
-int man()
-{
-    buku bukunya;
-    cout<<bukunya.setget ("Matematika");
-    return 0;
-}
