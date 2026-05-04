0ea06ba5acf83768755eccebd159bff7353ff361
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-04 12:57:56 +0700

    menambahkan constructor dengan parameter nim,  constructor dengan parameter nama, constructor dengan parameter nim dan nama

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -17,3 +17,16 @@ class mahasiswa{
 mahasiswa :: mahasiswa(){
 }
 
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; //definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; //definisi hanya nama
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; //definisi NIM dan NAMA
+    nama = iNama;
+}
+
