b8e63d6f66a9431caf2d27a0c353c517b0735314
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-04 13:02:43 +0700

    membuat objek mahasiswa dan memanggil constructor berparameter

--- constructorberpaameter.cpp
+++ constructorberpaameter.cpp
@@ -14,3 +14,7 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "Nama : " << nama << endl;
 }
 
+int main(){
+    mahasiswa mhs(12345, "Pascal"); //memangil constructor denan parameter
+    return 0;
+}
